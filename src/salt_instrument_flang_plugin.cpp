205ace617cfedc2b4a68b7683e71e72dc755a344
Nicholas Chaimov <nchaimov@paratools.com>
2024-12-05 15:29:47 -0800

    Add ProgramStmt handler, print Program name

--- src/salt_instrument_flang_plugin.cpp
+++ src/salt_instrument_flang_plugin.cpp
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/* SALT-FM Flang Fortran Instrumentor Plugin */
+
 #include "flang/Frontend/FrontendActions.h"
 #include "flang/Frontend/FrontendPluginRegistry.h"
 #include "flang/Parser/dump-parse-tree.h"
@@ -21,17 +23,36 @@ limitations under the License.
 
 using namespace Fortran::frontend;
 
+/**
+ * The main action of the Salt instrumentor.
+ * Visits each node in the parse tree.
+ */
 class SaltInstrumentAction : public PluginParseTreeAction {
-
-  // Visitor struct that defines Pre/Post functions for different types of nodes
   struct SaltInstrumentParseTreeVisitor {
+    // Default empty visit functions for otherwise unhandled types.
     template <typename A> bool Pre(const A &) { return true; }
     template <typename A> void Post(const A &) {}
+
+    // Override all types that we want to visit.
+    // Pre occurs when first visiting a node.
+    // Post occurs when returning from the node's children.
+    // See https://flang.llvm.org/docs/Parsing.html for information on the parse tree.
+    // There are three types of parse tree nodes:
+    // Wrappers, with a single data member, always named `v`.
+    // Tuples, encapsulating multiple values in a data member named `t` of type std::tuple.
+    // Discriminated unions, one of several types stored in data member named `u` of type std::variant.
+    // Use std::get() to retrieve value from `t` or `u`
 
     bool Pre(const Fortran::parser::FunctionSubprogram &) {
       isInSubprogram_ = true;
       return true;
     }
+
+    void Post(const Fortran::parser::ProgramStmt & program) {
+      llvm::outs() << "Program: \t"
+                   <<  program.v.ToString() << "\n";
+    }
+
     void Post(const Fortran::parser::FunctionStmt &f) {
       if (isInSubprogram_) {
         llvm::outs() << "Function:\t"
