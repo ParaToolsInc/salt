876eb1f5cdeef857d6f89caf626508899707ddfd
Izaak Beekman <zbeekman@gmail.com>
2024-12-06 09:44:13 -0500

    Update environment script for new TAU installed on UO machines

--- activate-salt-fm-env.sh
+++ activate-salt-fm-env.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+export SALT_ROOT=/storage/packages/salt-fm
+export PATH="$SALT_ROOT/base/tools/bin:$PATH"
+export PATH="$SALT_ROOT/opt/tau/x86_64/bin:$PATH"
+module use /storage/packages/salt-fm/spack/share/spack/modules/linux-rhel8-x86_64
+module use /storage/packages/salt-fm/spack/share/spack/modules/linux-centos7-x86_64/
 echo "purging loaded modules"
 module purge
 echo "listing loaded modules:"
@@ -7,7 +13,6 @@ echo "loading llvm and gcc:"
 module load llvm/git.086d8e6bb5daf8de43880ba90258c49e0fabf2c9_19.1.4-zpacv56
 #module load mpich/4.2.3-ugxzfxf
 module load gcc/14.2.0-ttkqi3s
-#module load tau/master-l3jx42k
 echo "listing loaded modules:"
 module list
 echo "Finished"
