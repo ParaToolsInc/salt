8f55e15e7d2fb7b930c15513cefc88ec3177337f
Izaak Beekman <zbeekman@gmail.com>
2024-12-09 09:04:38 -0800

    Update to use TAU module provided by Luke

--- activate-salt-fm-env.sh
+++ activate-salt-fm-env.sh
@@ -4,8 +4,8 @@
 # After this you can quickly configure, build, and test using `./build_and_test.sh`
 
 export SALT_ROOT=/storage/packages/salt-fm
-export PATH="$SALT_ROOT/base/tools/bin:$PATH"
-export PATH="$SALT_ROOT/opt/tau/x86_64/bin:$PATH"
+# export PATH="$SALT_ROOT/base/tools/bin:$PATH"
+module use /packages/salt-fm/modules
 module use /storage/packages/salt-fm/spack/share/spack/modules/linux-rhel8-x86_64
 module use /storage/packages/salt-fm/spack/share/spack/modules/linux-centos7-x86_64/
 echo "purging loaded modules"
@@ -17,6 +17,10 @@ echo "loading llvm and gcc:"
 module load llvm/git.086d8e6bb5daf8de43880ba90258c49e0fabf2c9_19.1.4-zpacv56
 #module load mpich/4.2.3-ugxzfxf
 module load gcc/14.2.0-ttkqi3s
+echo "loading ninja"
+module load ninja
+echo "loading tau"
+module load tau/2.34
 echo "listing loaded modules:"
 module list
 echo "Finished"
